e28d07eb39060076df8d18fc93a2c614471aeefa
unknown <msudhakar086@gmail.com>
2024-01-29 20:45:05 +0300

    prac

--- src/ConditionalRendering.tsx
+++ src/ConditionalRendering.tsx
@@ -1,8 +1,9 @@
 function Item({name,isPacked}){
     return(
         <li>
-            {name} {isPacked ? '  Yes' : ' No'}
+          <del> {name} {isPacked ?  ' ✔' : ''}</del>  
         </li>
+        
     );
 }
 
@@ -10,16 +11,78 @@ function Item({name,isPacked}){
 // Your components will often need to display different things depending on different conditions.
 // In React, you can conditionally render JSX using JavaScript syntax like if statements, &&, and ? : operators.
 
+function Drink({name}){
+    let part, caffeine, age;
+  if (name === 'tea') {
+    part = 'leaf';
+    caffeine = '15–70 mg/cup';
+    age = '4,000+ years';
+  } else if (name === 'coffee') {
+    part = 'bean';
+    caffeine = '80–185 mg/cup';
+    age = '1,000+ years';
+  }
+  return (
+    <section>
+      <h1>{name}</h1>
+      <dl>
+        <dt>Part of plant</dt>
+        <dd>{part}</dd>
+        <dt>Caffeine content</dt>
+        <dd>{caffeine}</dd>
+        <dt>Age</dt>
+        <dd>{age}</dd>
+      </dl>
+    </section>
+  );
+}
+
+
+function Drink1({name}){
+    const drink = {
+        tea: {
+            part : 'leaf',
+            caffeine : '15–70 mg/cup',
+            age : '4,000+ years'
+        } ,
+        coffee: {
+            part : 'bean',
+            caffeine : '80–185 mg/cup',
+            age : '1,000+ years'
+        }
+}
+
+const info = drink[name];
+  return (
+    <section>
+      <h1>{name}</h1>
+      <dl>
+        <dt>Part of plant</dt>
+        <dd>{info.part}</dd>
+        <dt>Caffeine content</dt>
+        <dd>{info.caffeine}</dd>
+        <dt>Age</dt>
+        <dd>{info.age}</dd>
+      </dl>
+    </section>
+  );
+}
+
  function ConditionalRendering(){
+    const count = 0;
     return(
         <div>
             <h1>hello</h1>
             <ul>
                 <Item name="apple" isPacked={true} />
                 <Item name="banana" isPacked={true} />
-                <Item name="orange" isPacked={false} />
-
+                <Item name="orange" isPacked={false} />                
             </ul>
+            <p>pear1 {true && '✔'} pear2</p>
+            <div> { count && <p>count : {count } </p> }</div>
+           <div> { count >= 0 && <p>count : {count } </p> }</div>
+         <p> if condition and table</p>  <Drink name="tea" />
+         <p> json and table</p>    <Drink1 name="coffee" />
         </div>
     );
 
